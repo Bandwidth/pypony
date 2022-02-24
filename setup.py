bc35129b2b7a858a5a8cd434c5ebed44ab6306db
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-02-24 15:06:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@ with open("requirements.txt", "r") as fp:
     requirements = fp.readlines()
 
 setup(
-    name="API Validator",
-    version="0.4.0",
-    py_modules=["api_validator"],
+    name='PyApi',
+    description='A python utility for contract testing APIs',
+    author='Bandwidth',
+    author_email='letstalk@bandwidth.com',
+    url='https://github.com/Bandwidth/pyapi/',
+    version='0.1.0',
+    py_modules=['pyapi'],
     install_requires=requirements,
 )
