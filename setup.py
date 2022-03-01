aed08657c1cfd694272d4df0d38801c012864b9e
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-03-01 16:36:14 -0500

    Modify `setup.py` so that package installs as a cli tool

--- setup.py
+++ setup.py
@@ -10,7 +10,11 @@ setup(
     author_email='letstalk@bandwidth.com',
     url='https://github.com/Bandwidth/pypony/',
     version='0.1.0',
-    py_modules=['pypony'],
+    py_modules=['pypony', 'src'],
     install_requires=requirements,
-    packages = find_packages("src", exclude=["website", "test"]),
+    packages = find_packages(exclude=["website", "test"]),
+    entry_points = '''
+        [console_scripts]
+        pypony=pypony:cli
+    '''
 )
