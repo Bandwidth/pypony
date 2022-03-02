d0ea2608b775714eac584ffb988e42521f7e546a
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-03-02 11:36:20 -0500

    Add long description to `setup.py`
    Add rich dependency to requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 with open("requirements.txt", "r") as fp:
     requirements = fp.readlines()
@@ -6,14 +10,16 @@ with open("requirements.txt", "r") as fp:
 setup(
     name='PyPony',
     description='A python utility for contract testing APIs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Bandwidth',
     author_email='letstalk@bandwidth.com',
     url='https://github.com/Bandwidth/pypony/',
     version='0.1.0',
     py_modules=['pypony', 'src'],
     install_requires=requirements,
-    packages = find_packages(exclude=["website", "test"]),
-    entry_points = '''
+    packages=find_packages(exclude=["website", "test"]),
+    entry_points='''
         [console_scripts]
         pypony=pypony:cli
     '''
