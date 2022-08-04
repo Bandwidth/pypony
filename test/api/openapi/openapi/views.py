a2fd3a1632208283599e6411801ee1ad7627265d
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-08-04 16:35:20 -0400

    Remove old tests

--- test/api/openapi/openapi/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-
-@api_view(('GET',))
-def index(request):
-    return Response({'message': request.GET.get('environment', None)})
