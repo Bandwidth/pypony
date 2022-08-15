3c5d5804135f1a67c9c7a9775d1eddb9e841645f
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-08-15 09:19:11 -0400

    Add Parsing Tests

--- tests/unit/test_errors.py
+++ tests/unit/test_errors.py
@@ -2,7 +2,16 @@ import pytest
 
 from src.errors import *
 
+# Test Values
 operation_set: set[str] = {"op_one", "op_two", "op_three"}
+coverage = 90.0
+desired_coverage = 100.0
+file_extension = ".json"
+base_context_value = "not env or steps"
+evaluation_error_message = "evaluation error"
+environment_variable_value = "NON_EXISTENT_ENV"
+expression_value = "2 + 2 = fish"
+schema_value = "application/xml"
 
 
 def test_undocumented_operation_error():
@@ -12,34 +21,34 @@ def test_undocumented_operation_error():
 
 def test_insufficient_coverage_error():
     with pytest.raises(InsufficientCoverageError) as err:
-        raise InsufficientCoverageError(90.0, 100.0, operation_set)
+        raise InsufficientCoverageError(coverage, desired_coverage, operation_set)
 
 
 def test_invalid_file_error():
     with pytest.raises(InvalidFileError) as err:
-        raise InvalidFileError(extension=".json")
+        raise InvalidFileError(file_extension)
 
 
 def test_base_context_error():
     with pytest.raises(BaseContextError) as err:
-        raise BaseContextError(value="not env or steps")
+        raise BaseContextError(base_context_value)
 
 
 def test_evaluation_error():
     with pytest.raises(EvaluationError) as err:
-        raise EvaluationError(message="evaluation error")
+        raise EvaluationError(evaluation_error_message)
 
 
 def test_environment_variable_error():
     with pytest.raises(EnvironmentVariableError) as err:
-        raise EnvironmentVariableError(value="NON_EXISTENT_ENV")
+        raise EnvironmentVariableError(environment_variable_value)
 
 
 def test_invalid_expression_error():
     with pytest.raises(InvalidExpressionError) as err:
-        raise InvalidExpressionError(value="2 + 2 = fish")
+        raise InvalidExpressionError(expression_value)
 
 
 def test_unsupported_schema_error():
     with pytest.raises(UnsupportedSchemaError) as err:
-        raise UnsupportedSchemaError(value="application/xml")
+        raise UnsupportedSchemaError(schema_value)
